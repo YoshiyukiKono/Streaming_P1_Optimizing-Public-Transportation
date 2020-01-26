139d5ea738e90935f17ac05f534a82b7effbaa4e
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2020-01-26 09:05:03 +0900

    Update faust_stream.py

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 # Faust will ingest records from Kafka in this format
+@dataclass
 class Station(faust.Record):
     stop_id: int
     direction_id: str
@@ -22,6 +23,7 @@ class Station(faust.Record):
 
 
 # Faust will produce records to Kafka in this format
+@dataclass
 class TransformedStation(faust.Record):
     station_id: int
     station_name: str
@@ -32,17 +34,23 @@ class TransformedStation(faust.Record):
 # TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
 #   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
+
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic("com.udacity.Station???", value_type=Station)
+
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic("com.udacity.TransformedStation???", partitions=1)
+
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+    "TransformedStation???",
+    default=int???,
+    partitions=1,
+    changelog_topic=out_topic,
+).hopping(
+    size=timedelta(minutes=1),
+    step=timedelta(seconds=10)
+)
 
 
 #
@@ -53,6 +61,21 @@ app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memor
 #
 #
 
+@app.agent(topic)
+async def station(station):
+    if station.red == True:
+        TODO
+        TransformedStation[s.station_id].line??? = 'red'
+    else if station.blue == True:
+        TODO
+        TransformedStation[s.station_id].line??? = 'blue'
+    else if station.green == True:
+        TODO
+        TransformedStation[s.station_id].line??? = 'green'
+        
+    async for s in station.group_by(Station.station_id):
+        TransformedStation[s.station_id] += s.number
+        print(f"{s.station_id}: {TransformedStation[s.station_id].current()}")
 
 if __name__ == "__main__":
     app.main()
