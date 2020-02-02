d121bf94b6aa98d23fa56435959fb768448bf9c6
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2020-02-02 14:26:34 +0900

    Update ksql.py

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -23,8 +23,8 @@ KSQL_URL = "http://localhost:8088"
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    timestamp LONG,
-    station_id DOUBLE,
+    timestamp DOUBLE,
+    station_id INTEGER,
     station_name VARCHAR,
     line VARCHAR
 ) WITH (
